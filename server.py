1fc33b0b9336cccf7b0d0924200ad953d1513f6a
alex1431999 <ah01242@surrey.ac.uk>
2019-11-22 22:03:35 +0000

    Import and register keyword blueprint

--- server.py
+++ server.py
@@ -4,4 +4,9 @@ This module holds the basic flask setup
 
 from flask import Flask
 
+from api.enpoints.keyword import keyword_blueprint
+
 app = Flask(__name__)
+
+# Register endpoints
+app.register_blueprint(keyword_blueprint)
