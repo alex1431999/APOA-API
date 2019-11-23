97f9f6f1780a6542e2edb6e99d50900f416ebaae
alex1431999 <ah01242@surrey.ac.uk>
2019-11-23 16:21:01 +0000

    Add username add keyword endpoint

--- api/enpoints/keyword.py
+++ api/enpoints/keyword.py
@@ -3,12 +3,15 @@ This module holds all the endpoints associated to keyword manipulation
 """
 
 from flask import Blueprint, request
+from flask_jwt_extended import get_jwt_identity, jwt_required
+
 from server import MONGO_CONTROLLER
 
 # Set up blueprint
 keyword_blueprint = Blueprint('keyword_endpoint', __name__)
 
 @keyword_blueprint.route('/keywords', methods=['GET', 'POST'])
+@jwt_required
 def keywords_route():
     """
     Handle all requests on /keywords
@@ -16,6 +19,8 @@ def keywords_route():
     GET: Get all keywords of a user
     POST: Add keyword
     """
+    username = get_jwt_identity()
+
     if request.method == 'GET':
         pass
     elif request.method == 'POST':
@@ -24,7 +29,7 @@ def keywords_route():
         language = request.json.get('language', None)
 
         try:
-            MONGO_CONTROLLER.add_keyword(keyword, language)
+            MONGO_CONTROLLER.add_keyword(keyword, language, username)
             return { 'msg': 'keyword successfully added' }, 200 # Successful
         except:
             return { 'msg': 'the request encountered an error' }, 400 # Bad request
