d522851ed84aeed5f3b9fbb06b24c8399c4d24b1
alex1431999 <ah01242@surrey.ac.uk>
2019-12-19 01:27:10 +0000

    Add Celery
    Send request to process keyword when keyword is added

--- server.py
+++ server.py
@@ -5,11 +5,17 @@ import os
 
 from common.mongo.controller import MongoController
 from flask_jwt_extended import JWTManager
+from celery import Celery
 from flask import Flask
 from flask_cors import CORS
 
 # Construct mongo controller
 MONGO_CONTROLLER = MongoController()
+
+# Setup celery
+celery_app = Celery('tasks',
+    broker = os.environ['BROKER_URL']
+)
 
 from api.enpoints.keyword import keyword_blueprint
 from api.enpoints.user import user_blueprint
