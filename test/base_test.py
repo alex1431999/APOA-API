6145a591f14da21584634afaacf3b28419a38f0e
alex1431999 <ah01242@surrey.ac.uk>
2020-04-10 23:23:48 +0100

    Add get_plotting_data tests

--- test/base_test.py
+++ test/base_test.py
@@ -3,11 +3,19 @@ This class defines the parent test class for all tests
 Every test shall inherit from this class
 """
 import unittest
+import os
+
+from common.mongo.controller import MongoController
+from common.config import SUPPORTED_LANGUAGES
 
 from server import app
 
 class BaseTest(unittest.TestCase):
     def setUp(self):
+        # Mongo
+        self.mongo_controller = MongoController(db_name='fyp_api_test')
+        self.register_users()
+
         # context setup
         self.app_context = app.app_context()
         self.app_context.push()
@@ -16,3 +24,22 @@ class BaseTest(unittest.TestCase):
 
     def tearDown(self):
         self.app_context.pop()
+        self.mongo_controller.client.drop_database(os.environ['MONGO_DATABASE_NAME'])
+
+    def register_users(self):
+        # Users
+        self.user_1 = { 'name': 'user_1', 'password': 'test' }
+        self.user_2 = { 'name': 'user_2', 'password': 'test' }
+
+        # Insert Users
+        self.mongo_controller.add_user(self.user_1['name'], self.user_1['password'])
+        self.mongo_controller.add_user(self.user_2['name'], self.user_2['password'])
+
+    def load_keyword_fixture(self, user):
+        keyword_string = 'some keyword'
+        language = SUPPORTED_LANGUAGES[0]
+
+        self.mongo_controller.add_keyword(keyword_string, language, user['name'])
+        keyword_id = self.mongo_controller.get_keyword(keyword_string, language)['_id']
+
+        return keyword_id
--- test/endpoints/test_crawl_endpoint.py
+++ test/endpoints/test_crawl_endpoint.py
@@ -1,13 +1,52 @@
 """
 This file holds all crawl endpoint tests
 """
+from flask_jwt_extended import create_access_token
 from bson import ObjectId
+from datetime import datetime
 
 from test.base_test import BaseTest
 
 class Test(BaseTest):
-    def test_plotting_data_no_data(self):
-        random_id = ObjectId
-        response = self.tester.get(f'/crawls/{random_id}/plotting_data')
+    def send_request(self, keyword_id, user):
+        access_token = create_access_token(user['name'])
+        headers = { 'Authorization': f'Bearer {access_token}' }
+
+        return self.tester.get(f'/crawls/{keyword_id}/plotting_data', headers=headers)
+
+    def test_plotting_data_no_keyword(self):
+        random_id = ObjectId()
+        response = self.send_request(random_id, self.user_1)
 
         self.assertEqual(response.status_code, 401, 'Request should have been rejected')
+
+    def test_plotting_data_invalid_user(self):
+        keyword_id = self.load_keyword_fixture(self.user_1)
+
+        response = self.send_request(keyword_id, self.user_2)
+
+        self.assertEqual(response.status_code, 401, 'Request should have been rejected')
+
+    def test_plotting_data_no_data(self):
+        keyword_id = self.load_keyword_fixture(self.user_1)
+
+        response = self.send_request(keyword_id, self.user_1)
+        response_data = response.get_json()
+
+        self.assertEqual(response_data, [], 'Since there is no plotting data, an empty list should be returned')
+
+    def test_plotting_data_with_data(self):
+        keyword_id = self.load_keyword_fixture(self.user_1)
+        timestamp = datetime.now().isoformat()
+        text = 'some text'
+        score = 15
+
+        tweet = self.mongo_controller.add_crawl_twitter(keyword_id, 123, 'some text', 15, 15, timestamp, return_object=True)
+        tweet = self.mongo_controller.set_score_crawl(tweet['_id'], score, return_object=True)
+
+        response = self.send_request(keyword_id, self.user_1)
+        response_data = response.get_json()
+
+        plotting_data_expected = [{ 'score': score, 'text': text, 'timestamp': timestamp}]
+        self.assertEqual(response_data, plotting_data_expected)
+
