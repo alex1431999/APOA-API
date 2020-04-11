da1e5ea6af4318528266071d1a83da19bd16c6c0
alex1431999 <ah01242@surrey.ac.uk>
2020-04-11 16:13:38 +0100

    Update get keywords test
    Add another test

--- test/endpoints/keyword/test_get_keywords.py
+++ test/endpoints/keyword/test_get_keywords.py
@@ -8,9 +8,7 @@ from test.base_test import BaseTest
 class Test(BaseTest):
     def setUp(self):
         super().setUp()
-
-        # Only register user_1 by default
-        self.mongo_controller.add_user(self.user_1['username'], self.user_1['password'])
+        self.register_users()
 
     def send_request(self, user):
         access_token = create_access_token(user['username'])
@@ -37,3 +35,15 @@ class Test(BaseTest):
 
         self.assertEqual(len(response_data), 1, 'Should have returned one keyword')
         self.assertEqual(response_data[0]['_id']['$oid'], str(keyword_id), 'The returned keyword should match the one inserted')
+
+    def test_get_keywords_wrong_user(self):
+        self.load_keyword_fixture(self.user_1) # Only user 1 has keywords
+
+        response = self.send_request(self.user_2) # But we are requesting keywords using user 2
+
+        self.assertEqual(response.status_code, 200, 'The request should still be valid')
+
+        expected_data = [] # User 2 should have 0 keywords
+        response_data = response.get_json()
+
+        self.assertEqual(expected_data, response_data, 'The data returned should match what was expected')
