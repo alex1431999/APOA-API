c20a010d4b1601436a9d5dbccc7aa8602b87db5b
alex1431999 <ah01242@surrey.ac.uk>
2020-04-13 20:26:12 +0100

    Add test langauges available

---
+++ test/endpoints/keyword/test_get_languages_available.py
@@ -0,0 +1,28 @@
+"""
+This file includes all tests for the available languages endpoint
+"""
+from flask_jwt_extended import create_access_token
+from common.config import SUPPORTED_LANGUAGES
+
+from test.base_test import BaseTest
+
+class Test(BaseTest):
+    def setUp(self):
+        super().setUp()
+        self.register_users()
+
+    def send_request(self, user):
+        access_token = create_access_token(user['username'])
+        headers = { 'Authorization': f'Bearer {access_token}' }
+        response = self.tester.get('/keywords/languages/available', headers=headers)
+
+        return response
+
+    def test_languages_available_content(self):
+        response = self.send_request(self.user_1)
+
+        self.assertEqual(response.status_code, 200, 'Should have been authorized')
+
+        response_data = response.get_json()
+
+        self.assertEqual(response_data, SUPPORTED_LANGUAGES, 'The langauges returned should equal the ones in the common library')
