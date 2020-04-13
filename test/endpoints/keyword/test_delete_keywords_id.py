a6253c6c4284eba961aa5e1255af89ce9125c11d
alex1431999 <ah01242@surrey.ac.uk>
2020-04-13 20:07:24 +0100

    Add delete keyword by ID tests

---
+++ test/endpoints/keyword/test_delete_keywords_id.py
@@ -0,0 +1,33 @@
+"""
+This file includes all tests for the delete keyword by id endpoint
+"""
+from flask_jwt_extended import create_access_token
+from bson import ObjectId
+
+from test.base_test import BaseTest
+
+class Test(BaseTest):
+    def setUp(self):
+        super().setUp()
+        self.register_users()
+
+    def send_request(self, user, _id):
+        access_token = create_access_token(user['username'])
+        headers = { 'Authorization': f'Bearer {access_token}' }
+        response = self.tester.delete(f'/keywords/{str(_id)}', headers=headers)
+
+        return response
+
+    def test_delete_keywords_id_unauthorized(self):
+        random_id = ObjectId()
+
+        response = self.send_request(self.user_1, random_id)
+
+        self.assertEqual(response.status_code, 400, 'The user should have not been authorized to delete the keyword')
+
+    def test_delete_keywords_id_authorized(self):
+        keyword_id = self.load_keyword_fixture(self.user_1)
+
+        response = self.send_request(self.user_1, keyword_id)
+
+        self.assertEqual(response.status_code, 200, 'Should have been authorized')
