bab1aabdddda0bb19eb4e165cfd9baf54c5ba8e7
alex1431999 <alexhaller99@yahoo.de>
2020-09-27 09:51:01 +0200

    Feature: add static endpoints (team -> alex.jpg + anthony.jpg)

--- api/endpoints/static.py
+++ api/endpoints/static.py
@@ -13,6 +13,16 @@ def get_apoa_logo():
     return send_file("assets/images/apoa/logo.svg")
 
 
+@static_endpoint.route("/static/apoa/team/alex", methods=["GET"])
+def get_apoa_team_alex():
+    return send_file("assets/images/apoa/team/alex.jpg")
+
+
+@static_endpoint.route("/static/apoa/team/anthony", methods=["GET"])
+def get_apoa_team_anthony():
+    return send_file("assets/images/apoa/team/anthony.jpg")
+
+
 @static_endpoint.route("/static/isa/logo", methods=["GET"])
 def get_isa_logo():
     return send_file("assets/images/isa/logo.svg")
